a807de0f1caaa7f8928982b364c939735564ba0b
paulbud61 <paulbud61@gmail.com>
2024-05-22 12:06:53 +0300

    update

---
+++ contacts.js
@@ -0,0 +1,21 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+// TODO: documentare fiecare funcție
+function listContacts() {
+  // ...codul tău
+}
+
+function getContactById(contactId) {
+  // ...codul tău
+}
+
+function removeContact(contactId) {
+  // ...codul tău
+}
+
+function addContact(name, email, phone) {
+  // ...codul tău
+}
